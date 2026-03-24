052cd1d7db1da35b8c85ff8f443347ead7c160f0
Saurabh Kumar <saurabh.k0402@gmail.com>
2026-03-24 22:49:48 +0530

    feat: add logic to play one of the two audio

--- tussi-na-jao/main.py
+++ tussi-na-jao/main.py
@@ -15,17 +15,43 @@ class Player():
 class Angle_handler():
     def __init__(self, player):
         self.player = player
-        self.last_alert_angle = None
+        self.last_played = None
+        self.angle = None
 
     def __call__(self, angle):
         print(f"Lid angle changed: {angle:.1f}°")
-        self.player.play("./audio/please_dont_go.mp3")
-        self.last_alert_angle = angle
+
+        if not self.angle:
+            self.angle = max(0, angle - 10)
+            return
+
+        if self.angle - angle < 10:
+            if self.angle < angle:
+                self.angle = angle
+                self.last_played = None
+            return
+
+        if angle < 60:
+            if self.last_played == "go_away":
+                return
+            
+            self.player.play("./audio/go_away.mp3")
+            self.last_played = "go_away"
+            return
+
+        if angle < 90:
+            if self.last_played == "please_dont_go":
+                return
+            
+            self.player.play("./audio/please_dont_go.mp3")
+            self.last_played = "please_dont_go"
+
+        self.angle = angle
 
 if __name__ == "__main__":
     player = Player()
     handle_angle_change = Angle_handler(player)
 
     with LidSensor() as sensor:
-        for angle in sensor.monitor(callback=handle_angle_change, interval=0.5):
+        for angle in sensor.monitor(callback=handle_angle_change, interval=1):
             pass
